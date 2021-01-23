6e8f263fdf66b1f0c818886577c44ebf483eb6e3
Santiago <santiagoln09@gmail.com>
2021-01-23 17:38:15 -0500

    deleted unnecesay files

--- prueba.js
+++
@@ -1,7 +0,0 @@
-const desc = "Mouse Ergonómico de pilas, Plug and Play: Sólo conectar el receptor nano USB incluido en el puerto USB de tu computadora, sin driver adicional necesario, y pronto listo para disfrutar de la comodidad."
-
-console.log(desc.length);
-
-
-const shortDesc = desc.substring(0,100) + "(...)"
-console.log(shortDesc)
